677b08b296dd2826ff662a1da8403f73ebcce361
Elcand <tjandraraharja99@gmail.com>
2025-03-05 14:04:32 +0700

    Refactor UI on admin paage

--- resources/views/admin/hero/index.blade.php
+++ resources/views/admin/hero/index.blade.php
@@ -5,11 +5,11 @@
     <section>
         <div class="container mt-8">
             <div class="row ps-3">
+                <h1 class="text-gray-900 text-2xl font-bold ps-4">Manage Hero Section
+                </h1>
                 <div class="col-md-12 mt-3">
                     <div class="card border-0 shadow-sm rounded">
                         <div class="card-body">
-                            <h1 class="text-gray-900 text-2xl font-bold ps-4 mb-3 ">Manage Hero Section
-                            </h1>
                             <hr>
                             @if (session('success'))
                                 <div class="alert alert-success mt-4 delay-200">
@@ -17,20 +17,20 @@
                                 </div>
                             @endif
                             <form action="{{ route('admin.hero.storeOrUpdate') }}" method="POST"
-                                enctype="multipart/form-data" class="mt-6">
+                                enctype="multipart/form-data">
                                 @csrf
                                 <div class="mb-3">
                                     @if (!empty($hero->image))
                                         <div class="mt-3">
                                             <p class="text-gray-700 dark:text-gray-300">This Background:</p>
-                                            <img src="{{ asset(Str::startsWith($hero->image, 'hero/') ? 'storage/' . $hero->image : 'assets/img/' . $hero->image) }}"
+                                            <img src="{{ asset('storage/hero/' . $hero->image) }}"
                                                 alt="Background Image"
                                                 class="img-fluid rounded-lg border border-gray-300 dark:border-gray-600"
                                                 style="max-height: 200px; object-fit: cover;">
                                         </div>
                                     @endif
                                     <label for="formFile" class="form-label mt-4">Upload image</label>
-                                    <input class="form-control" type="file" id="formFile" name="image"
+                                    <input class="form-control" type="file" id="formFile"
                                         placeholder="Upload Image">
                                     @error('image')
                                         <div class="invalid-feedback mt-3 text-red-500 text-sm">
@@ -38,7 +38,6 @@
                                         </div>
                                     @enderror
                                 </div>
-
                                 <div class="mb-3">
                                     <label for="name"
                                         class="form-label block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
